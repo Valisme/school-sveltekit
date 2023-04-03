7ab8e1f4a37260fee5cce4e2579e9085dfed4419
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-04-03 15:45:32 +0700

    Add navbar🧭

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
-    import "../app.css"
+	import '../app.css';
 </script>
-<slot/>
+
+<nav class="bg-lime-600 w-auto h-14 flex flex-row items-center p-6 font text-xl text-white">
+	<ul class="flex flex-row gap-4">
+		<li><a href="/">Home</a></li>
+		<li><a href="/class">Class</a></li>
+		<li><a href="/forum">Forum</a></li>
+	</ul>
+	<ul class="ml-auto flex flex-row gap-4">
+		<li>Sign In</li>
+		<li>Login</li>
+	</ul>
+</nav>
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.teal.950);
+	}
+</style>
