ea21e3124c21b745d607a6abc1bb8c779db27137
Valmero Ahmadtofa <valism3ro@gmail.com>
2023-04-03 15:46:45 +0700

    Add simple landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+    import {blur} from 'svelte/transition'
+	const welcome: string = "Welcome to School's Website";
+	let typedWelcome: string = '';
+	let index: number = 0;
+    let typed:boolean = false;
+	onMount( () => {
+		const typing = setInterval(async() => {
+			if (index < welcome.length) {
+				typedWelcome += welcome[index];
+				index++;
+			} else {
+                clearInterval(typing)
+                typed = true;
+				return;
+			}
+		},50);
+	});
+</script>
+
+<div class="flex flex-col">
+    <div class="h-96 flex flex-col items-center gap-4 justify-center ">
+        <p class="text-7xl font-semibold text-white text-center">{typedWelcome}</p>
+        {#if typed}
+        <p class="text-3xl font-medium text-white text-center mt-6transition-all duration-0 ease-linear" transition:blur="{{amount:10}}">Education is key for <span class="text-yellow-400">Bright</span> Future</p>
+        {/if}
+    </div>
+</div>
